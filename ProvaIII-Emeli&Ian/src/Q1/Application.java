c78c6d803a7dcd581866474ab8f117c541c340a5
Emy Batista <80564446+EmyBatista21@users.noreply.github.com>
2023-12-14 23:36:55 -0300

    Update Application.java

--- ProvaIII-Emeli&Ian/src/Q1/Application.java
+++ ProvaIII-Emeli&Ian/src/Q1/Application.java
@@ -1,5 +1,9 @@
 package Q1;
-
+/*
+O padrão Builder foi o escolhido para simplificar a criação de objetos complexos, como o Perfil, que têm partes obrigatórias 
+e opcionais. Ele oferece flexibilidade na configuração do perfil, evitando a complexidade de um construtor com muitos parâmetros. 
+Essa escolha torna a construção do perfil mais simples, modular e fácil de entender.
+* */
 public class Application {
     public static void main(String[] args) {
         // Cria um builder concreto
