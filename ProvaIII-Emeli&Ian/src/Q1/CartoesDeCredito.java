9fa1a2b16feb16a31a5f0c30bab8a4f196a0ff99
Emy Batista <80564446+EmyBatista21@users.noreply.github.com>
2023-12-14 23:37:12 -0300

    Update CartoesDeCredito.java

--- ProvaIII-Emeli&Ian/src/Q1/CartoesDeCredito.java
+++ ProvaIII-Emeli&Ian/src/Q1/CartoesDeCredito.java
@@ -1,6 +1,10 @@
 package Q1;
 
-//Classe para representar um Cartão de Crédito
+/*
+O padrão Builder foi o escolhido para simplificar a criação de objetos complexos, como o Perfil, que têm partes obrigatórias 
+e opcionais. Ele oferece flexibilidade na configuração do perfil, evitando a complexidade de um construtor com muitos parâmetros. 
+Essa escolha torna a construção do perfil mais simples, modular e fácil de entender.
+* */
 public class CartoesDeCredito {
  private String numero;
 
